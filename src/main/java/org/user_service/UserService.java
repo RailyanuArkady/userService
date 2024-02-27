47342b0d8fa877be408c2fab5734ac58950c7af8
RexArr <kukusmaner@gmail.com>
2024-02-27 21:47:47 +0300

    Task1 added Hello World

--- src/main/java/org/user_service/UserService.java
+++ src/main/java/org/user_service/UserService.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserService {
+
     public static void main(String[] args) {
         SpringApplication.run(UserService.class, args);
+        System.out.println("Hello World!");
     }
 }
