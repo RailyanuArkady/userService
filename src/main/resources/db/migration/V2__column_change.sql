3bb5f987b5aff105caf5c489bc0e33956ae94af9
RexArr <kukusmaner@gmail.com>
2024-03-06 21:35:27 +0300

    Task6 alter data type of sex column

--- src/main/resources/db/migration/V2__column_change.sql
+++ src/main/resources/db/migration/V2__column_change.sql
@@ -5,14 +5,5 @@ ALTER COLUMN modified_at SET NOT NULL;
 ALTER TABLE users
 ALTER COLUMN modified_at SET NOT NULL;
 
-ALTER TABLE users ADD COLUMN sex_tmp varchar(12);
-
-UPDATE users SET sex_tmp = sex;
-
-ALTER TABLE users DROP COLUMN sex;
-
-ALTER TABLE users ADD COLUMN sex varchar(12);
-
-UPDATE users SET sex = sex_tmp;
-
-ALTER TABLE users DROP COLUMN sex_tmp;
+ALTER TABLE users
+ALTER COLUMN sex TYPE varchar(12);
