d90c69772cd923e01412f6ac911b34ba322b8c09
RexArr <kukusmaner@gmail.com>
2024-03-22 19:56:17 +0300

    Task12 added tests

--- src/test/java/org/user_service/services/unit/UserServiceTest.java
+++ src/test/java/org/user_service/services/unit/UserServiceTest.java
@@ -1,20 +1,26 @@
 package org.user_service.services.unit;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.user_service.dto.request.UserRequestDTO;
 import org.user_service.dto.response.UserResponseDTO;
 import org.user_service.mapper.UserMapper;
+import org.user_service.model.PersonSex;
 import org.user_service.model.User;
 import org.user_service.repository.UserRepository;
 import org.user_service.services.UserService;
 import org.user_service.services.util.TestUtils;
 
 import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.user_service.services.util.TestUtils.MOCKED_ID;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -30,45 +36,53 @@ class UserServiceTest {
         User user = TestUtils.buildUser();
         UserResponseDTO expectedUserResponseDto = TestUtils.buildUserResponseDTO();
 
-        Mockito.doReturn(Optional.of(user)).when(userRepository).findById(TestUtils.MOCKED_ID);
+        Mockito.doReturn(Optional.of(user)).when(userRepository).findById(MOCKED_ID);
         Mockito.doReturn(expectedUserResponseDto).when(userMapper).userToResponseDTO(user);
 
-        UserResponseDTO actualUser = userService.findUserById(TestUtils.MOCKED_ID);
+        UserResponseDTO actualUser = userService.findUserById(MOCKED_ID);
 
-        Assertions.assertEquals(expectedUserResponseDto, actualUser);
+        assertEquals(expectedUserResponseDto, actualUser);
     }
 
-//    @Test
-//    void TestSaveUser() {
-//        when(userMapper.dtoToUser(userRequestDTO)).thenReturn(user);
-//        when(userRepository.save(user)).thenReturn(user);
-//
-//        UUID externalId = userService.saveUser(userRequestDTO);
-//
-//        verify(userRepository, times(1)).save(user);
-//        assertEquals(user.getExternalId(), externalId);
-//    }
-//
-//
-//    @Test
-//    void TestUpdateUser() {
-//
-//        when(userRepository.findById(USER_ID)).thenReturn(Optional.ofNullable(user));
-//
-//        userResponseDTO = userService.updateUser(USER_ID, userRequestDTO);
-//
-//        verify(userMapper, times(1)).updateUserFromDTO(userRequestDTO, user);
-//        assertEquals(PersonSex.MALE, userResponseDTO.sex());
-//    }
-//
-//    @Test
-//    void TestDeleteUser() {
-//        when(userRepository.findById(USER_ID)).thenReturn(Optional.ofNullable(user));
-//        userService.deleteUser(USER_ID);
-//        assertAll(
-//                () -> assertTrue(user.isDeleted()),
-//                () -> assertTrue(Optional.of(user.getPassport()).isPresent())
-//        );
-//
-//    }
+    @Test
+    void TestSaveUser() {
+        User user = TestUtils.buildUser();
+        UserRequestDTO userRequestDTO = TestUtils.buildUserRequestDTO();
+
+        doReturn(user).when(userMapper).dtoToUser(userRequestDTO);
+        doReturn(user).when(userRepository).save(user);
+
+        UUID externalId = userService.saveUser(userRequestDTO);
+
+        assertEquals(user.getExternalId(), externalId);
+    }
+
+    @Test
+    void TestUpdateUser() {
+        User user = TestUtils.buildUser();
+        UserRequestDTO userRequestDTO = TestUtils.buildUserRequestDTO();
+        UserResponseDTO userResponseDTO = TestUtils.buildUserResponseDTO();
+
+        doReturn(Optional.of(user)).when(userRepository).findById(MOCKED_ID);
+        doNothing().when(userMapper).updateUserFromDTO(userRequestDTO, user);
+        doReturn(userResponseDTO).when(userMapper).userToResponseDTO(user);
+
+        UserResponseDTO userResponseDTOActual = userService.updateUser(MOCKED_ID, userRequestDTO);
+
+        assertEquals(PersonSex.MALE, userResponseDTOActual.sex());
+    }
+
+    @Test
+    void TestDeleteUser() {
+        User user = TestUtils.buildUser();
+
+        doReturn(Optional.ofNullable(user)).when(userRepository).findById(MOCKED_ID);
+        userService.deleteUser(MOCKED_ID);
+
+        assertAll(
+                () -> assertTrue(user.isDeleted()),
+                () -> assertTrue(Optional.of(user.getPassport()).isPresent())
+        );
+
+    }
 }
