73b472d682166786bbf07988dcefebdea94efce2
RexArr <kukusmaner@gmail.com>
2024-03-22 19:56:17 +0300

    Task12 added tests

--- src/test/java/org/user_service/services/unit/UserServiceTest.java
+++ src/test/java/org/user_service/services/unit/UserServiceTest.java
@@ -1,5 +1,6 @@
 package org.user_service.services.unit;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
