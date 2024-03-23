f3578863ac8f32aa637049e88d6dd3138a85e031
RexArr <kukusmaner@gmail.com>
2024-03-23 17:51:29 +0300

    Task13 swagger description added

--- src/main/java/org/user_service/controller/UserController.java
+++ src/main/java/org/user_service/controller/UserController.java
@@ -1,5 +1,6 @@
 package org.user_service.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,24 +17,28 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "User and passport post method", description = "Returns user UUID")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CreateUserResponseDTO createUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
         return new CreateUserResponseDTO(userService.saveUser(userRequestDTO));
     }
 
+    @Operation(summary = "Get User by id", description = "Returns user and passport found by id")
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public UserResponseDTO getUserById(@PathVariable Long id) {
         return userService.findUserById(id);
     }
 
+    @Operation(summary = "Update User with passed id", description = "Returns updated user and password")
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public UserResponseDTO updateUserByID(@PathVariable Long id, @Valid @RequestBody UserRequestDTO userRequestDTO) {
         return userService.updateUser(id, userRequestDTO);
     }
 
+    @Operation(summary = "Soft delete user with passed id")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteUser (@PathVariable Long id){
