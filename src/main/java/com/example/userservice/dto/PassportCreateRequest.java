0ea9ed7b12ad24300b8415ada61f1818429363b5
Дарья Ямщикова <dashaa1607@gmail.com>
2025-10-05 15:09:16 +0300

    Task7 билд MapStruct

--- src/main/java/com/example/userservice/dto/PassportCreateRequest.java
+++ src/main/java/com/example/userservice/dto/PassportCreateRequest.java
@@ -3,12 +3,14 @@ package com.example.userservice.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalDate;
 
 public record PassportCreateRequest(
-        //паттерны на серию и номер
+        @Pattern(regexp = "\\d{4}", message = "Invalid passport series: must be 4 digits")
         @NotBlank String passportSeries,
+        @Pattern(regexp = "\\d{6}", message = "Invalid passport number: must be 6 digits")
         @NotBlank String passportNumber,
         @NotBlank String passportDivisionName,
         @NotBlank String passportDivisionCode,
--- src/main/java/com/example/userservice/mapper/UserMapper.java
+++ src/main/java/com/example/userservice/mapper/UserMapper.java
@@ -2,23 +2,29 @@ package com.example.userservice.mapper;
 
 import com.example.userservice.dto.UserCreateRequest;
 import com.example.userservice.dto.UserCreateResponse;
+import com.example.userservice.entities.Passport;
 import com.example.userservice.entities.Users;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-//дополнить маппер, полями isDeleted и externalId должны прямо тут выставлять, проверить мапиться ли паспорт, скорее всего нет для этого над классом надо еще добавить uses = {}
-@Mapper(componentModel = "spring")
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = PassportMapper.class)
 public interface UserMapper {
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "externalId", ignore = true)
-    @Mapping(target = "isDeleted", ignore = true)
-    @Mapping(target = "passports", ignore = true)
+    @Mapping(target = "externalId", expression = "java(java.util.UUID.randomUUID())")
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "modifiedAt", ignore = true)
     @Mapping(target = "photoUrl", source = "photoId")
     Users toEntity(UserCreateRequest request);
-
-    //не используется
     UserCreateResponse toResponse(Users user);
+
+    default Users toResponsePass(UserCreateRequest request, PassportMapper passportMapper) {
+        Users user = toEntity(request);
+        Passport passport = passportMapper.toEntity(request.passport());
+        passport.setUser(user);
+        user.setPassports(List.of(passport));
+        return user;
+    }
 }
